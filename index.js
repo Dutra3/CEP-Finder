e969f1bb6bd42150f7ac593cd0d7b7124988ad0e
Gabriel <gabrieldutra45@gmail.com>
2022-07-16 19:55:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const cep = document.querySelector('#cep')
+const search = document.querySelector('#search')
+const clean = document.querySelector('#clean')
+const response = document.querySelector('#response')
+
+const fillAddress = (address) => {
+    response.classList.remove('hide')
+    document.getElementById('street').value = address.logradouro;
+    document.getElementById('city').value = address.localidade;
+    document.getElementById('state').value = address.uf;
+}
+
+cep.addEventListener('keypress', (newCep) => cepMask(newCep.target.value))
+
+const cepMask = (newCep) => {
+    newCep = newCep.replace(/\D/g, "")
+    newCep = newCep.replace(/(\d{5})(\d{3})/,'$1-$2')
+    cep.value = newCep
+}
+
+const validCep = (cep) => cep.length == 8 && /^[0-9]+$/.test(cep);
+
+search.addEventListener('click', async() => {
+    const cepValue = cep.value;
+    const url = `https://viacep.com.br/ws/${cepValue}/json/`;
+    if (validCep(cepValue)) {
+        const data = await fetch(url);
+        const address = await data.json();
+        if(address.hasOwnProperty('erro')) {
+            alert('CEP incorreto')
+        } else {
+            fillAddress(address);
+        }
+    } else {
+        alert('CEP inválido')
+    }
+})
+
+clean.addEventListener('click', () => {
+    document.getElementById('cep').value = '';
+    document.getElementById('street').value = '';
+    document.getElementById('city').value = '';
+    document.getElementById('state').value = '';
+    response.classList.add('hide')
+})
+
+//  /([0-9]{5})([0-9]{3})/,'$1-$2'
+// /(\d{5})(\d{3})/,'$1-$2'
+// ([0-9]{5})[-]([0-9]{3})
+// cep.replace(/^(\d{5})(\d{3})/,'$1-$2')
+// .replace(/\D/g, '')
+
